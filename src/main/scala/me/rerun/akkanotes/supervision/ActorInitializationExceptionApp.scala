3df5a86b2cb938c9552815c3c45765b224095642
Arun Manivannan <arun@arunma.com>
2014-11-10 17:47:06 +0800

    Akka Supervision

--- src/main/scala/me/rerun/akkanotes/supervision/ActorInitializationExceptionApp.scala
+++ src/main/scala/me/rerun/akkanotes/supervision/ActorInitializationExceptionApp.scala
@@ -8,11 +8,11 @@ import akka.actor.ActorLogging
 object ActorInitializationExceptionApp extends App{
 
   val actorSystem=ActorSystem("ActorInitializationException")
-  val actor=actorSystem.actorOf(Props[ActorInitializationException])
+  val actor=actorSystem.actorOf(Props[ActorInitializationExceptionActor], "initializationExceptionActor")
   actor!"someMessageThatWillGoToDeadLetter"
 }
 
-class ActorInitializationException extends Actor with ActorLogging{
+class ActorInitializationExceptionActor extends Actor with ActorLogging{
   override def preStart={
     throw new Exception("Some random exception")
   }
